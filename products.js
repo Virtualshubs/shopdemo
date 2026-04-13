85f52a732fb76fcd6f7fbe6818c5f1ea1b84c235
Virtualshubs <mmateriaa@gmail.com>
2026-04-13 15:32:02 +0200

    Update products.js

--- products.js
+++ products.js
@@ -1,29 +1,29 @@
 const PRODUCTS = [
   {
     id: "lunar-white",
-    scene: "aHR0cHM6Ly9wcm9kdWN0...",
+    scene: "aHR0cHM6Ly9wcm9kdWN0LjNkdHdpbnMudGVjaC8/bmFtZT1Tb2YlQzMlQTEmY29uZmlnPUJhc2VfbWV0JUMzJUExbGljYSUzQSUyMzAzMDQyYiU3Q0VzdHJ1Y3R1cmFfYmFzZSUzQSUyMzE3MTcxNyU3Q0VzdHJ1Y3R1cmFfaW50ZXJuYSUzQSUyMzFmMWYxZiU3Q0VzdHJ1Y3R1cmFfZXh0ZXJuYSUzQSUyMzEyMTIxMiU3Q0NvamluZXNfYXV4aWxpYXJlcyUzQSUyMzE0MTQxNCU3Q0FzaWVudG9zJTNBJTIzMDgwODA4JnF1YW50aXR5PTEmdG9rZW49MDk4ZDM1ZmUtNDBkZC00MzZlLWIyNDUtNWMzZjA4NWJhYWIxJm1vZGVsVXJsPWh0dHBzJTNBJTJGJTJGdmlld2VyLjNkdHdpbnMudGVjaCUyRlNvZmElQ0MlODEuZ2xiJmJ1eVVSTD1odHRwcyUzQSUyRiUyRjNkdHdpbnMuZXM=",
     title: "Lunar White",
     price: "129€",
-    image: "https://cdn.shopify.com/....png",
+    image: "https://cdn.shopify.com/s/files/1/0948/5366/1017/files/CapturadePantalla2026-03-24alas14.25.40.png?v=1774358754",
     meta: "On demand",
     type: "vh" // sidebar
   },
   {
     id: "orbit-grey",
-    scene: "aHR0cHM6Ly9wcm9kdWN0...",
+    scene: "aHR0cHM6Ly9wcm9kdWN0LjNkdHdpbnMudGVjaC8%2FbmFtZT1Cb2xzbyZjb25maWc9Y2FtYmlhcl9jb2xvciUzQSUyM2FkMDAwMCZxdWFudGl0eT0xJnRva2VuPTFjMGJhNTVhLTZiYjUtNDhhMi1iNDRjLWIyYzFmN2JkNjFlNyZtb2RlbFVybD1odHRwcyUzQSUyRiUyRnZpZXdlci4zZHR3aW5zLnRlY2glMkZib2xzb3BpZWwuZ2xiJmJ1eVVSTD1odHRwcyUzQSUyRiUyRjNkdHdpbnMuZXMlMkY%3D",
     title: "Orbit Grey",
     price: "129€",
-    image: "https://cdn.shopify.com/....png",
+    image: "https://cdn.shopify.com/s/files/1/0948/5366/1017/files/CapturadePantalla2026-03-24alas14.25.40.png?v=1774358754",
     meta: "On demand",
     type: "vh"
   },
 
   {
     id: "jupiter",
-    scene: "aHR0cHM6Ly9wcm9kdWN0...",
+    scene: "aHR0cHM6Ly9wcm9kdWN0LjNkdHdpbnMudGVjaC8/bmFtZT1Tb2YlQzMlQTEmY29uZmlnPUJhc2VfbWV0JUMzJUExbGljYSUzQSUyMzAzMDQyYiU3Q0VzdHJ1Y3R1cmFfYmFzZSUzQSUyMzE3MTcxNyU3Q0VzdHJ1Y3R1cmFfaW50ZXJuYSUzQSUyMzFmMWYxZiU3Q0VzdHJ1Y3R1cmFfZXh0ZXJuYSUzQSUyMzEyMTIxMiU3Q0NvamluZXNfYXV4aWxpYXJlcyUzQSUyMzE0MTQxNCU3Q0FzaWVudG9zJTNBJTIzMDgwODA4JnF1YW50aXR5PTEmdG9rZW49MDk4ZDM1ZmUtNDBkZC00MzZlLWIyNDUtNWMzZjA4NWJhYWIxJm1vZGVsVXJsPWh0dHBzJTNBJTJGJTJGdmlld2VyLjNkdHdpbnMudGVjaCUyRlNvZmElQ0MlODEuZ2xiJmJ1eVVSTD1odHRwcyUzQSUyRiUyRjNkdHdpbnMuZXM=",
     title: "Júpiter",
     price: "129€",
-    image: "https://cdn.shopify.com/....png",
+    image: "https://cdn.shopify.com/s/files/1/0948/5366/1017/files/20260115_0053_Image_Generation_remix_01kezestsbfs4avdn5p9enm423.png?v=1774642016",
     type: "grid"
   }
 ];
