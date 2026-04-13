8b296953bbc6e637117b16d9856fbc08e3e50c9c
Virtualshubs <mmateriaa@gmail.com>
2026-04-13 15:30:18 +0200

    Create products.js

---
+++ products.js
@@ -0,0 +1,29 @@
+const PRODUCTS = [
+  {
+    id: "lunar-white",
+    scene: "aHR0cHM6Ly9wcm9kdWN0...",
+    title: "Lunar White",
+    price: "129€",
+    image: "https://cdn.shopify.com/....png",
+    meta: "On demand",
+    type: "vh" // sidebar
+  },
+  {
+    id: "orbit-grey",
+    scene: "aHR0cHM6Ly9wcm9kdWN0...",
+    title: "Orbit Grey",
+    price: "129€",
+    image: "https://cdn.shopify.com/....png",
+    meta: "On demand",
+    type: "vh"
+  },
+
+  {
+    id: "jupiter",
+    scene: "aHR0cHM6Ly9wcm9kdWN0...",
+    title: "Júpiter",
+    price: "129€",
+    image: "https://cdn.shopify.com/....png",
+    type: "grid"
+  }
+];
