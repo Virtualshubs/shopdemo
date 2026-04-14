21b277ac86fcf1e0b45cb13db8260ab0ca920a59
Virtualshubs <mmateriaa@gmail.com>
2026-04-14 18:44:31 +0200

    Update product IDs and titles in products.js

--- products.js
+++ products.js
@@ -9,9 +9,9 @@ const PRODUCTS = [
     type: "vh" // sidebar
   },
   {
-    id: "orbit-grey",
+    id: "Bolso",
     scene: "aHR0cHM6Ly9wcm9kdWN0LjNkdHdpbnMudGVjaC8%2FbmFtZT1Cb2xzbyZjb25maWc9Y2FtYmlhcl9jb2xvciUzQSUyM2FkMDAwMCZxdWFudGl0eT0xJnRva2VuPTFjMGJhNTVhLTZiYjUtNDhhMi1iNDRjLWIyYzFmN2JkNjFlNyZtb2RlbFVybD1odHRwcyUzQSUyRiUyRnZpZXdlci4zZHR3aW5zLnRlY2glMkZib2xzb3BpZWwuZ2xiJmJ1eVVSTD1odHRwcyUzQSUyRiUyRjNkdHdpbnMuZXMlMkY%3D",
-    title: "Orbit Grey",
+    title: "Limo",
     price: "129€",
     image: "https://cdn.shopify.com/s/files/1/0948/5366/1017/files/CapturadePantalla2026-03-24alas14.25.40.png?v=1774358754",
     meta: "On demand",
@@ -19,9 +19,9 @@ const PRODUCTS = [
   },
 
   {
-    id: "jupiter",
+    id: "Sofá",
     scene: "aHR0cHM6Ly9wcm9kdWN0LjNkdHdpbnMudGVjaC8/bmFtZT1Tb2YlQzMlQTEmY29uZmlnPUJhc2VfbWV0JUMzJUExbGljYSUzQSUyMzAzMDQyYiU3Q0VzdHJ1Y3R1cmFfYmFzZSUzQSUyMzE3MTcxNyU3Q0VzdHJ1Y3R1cmFfaW50ZXJuYSUzQSUyMzFmMWYxZiU3Q0VzdHJ1Y3R1cmFfZXh0ZXJuYSUzQSUyMzEyMTIxMiU3Q0NvamluZXNfYXV4aWxpYXJlcyUzQSUyMzE0MTQxNCU3Q0FzaWVudG9zJTNBJTIzMDgwODA4JnF1YW50aXR5PTEmdG9rZW49MDk4ZDM1ZmUtNDBkZC00MzZlLWIyNDUtNWMzZjA4NWJhYWIxJm1vZGVsVXJsPWh0dHBzJTNBJTJGJTJGdmlld2VyLjNkdHdpbnMudGVjaCUyRlNvZmElQ0MlODEuZ2xiJmJ1eVVSTD1odHRwcyUzQSUyRiUyRjNkdHdpbnMuZXM=",
-    title: "Júpiter",
+    title: "Sofá",
     price: "129€",
     image: "https://cdn.shopify.com/s/files/1/0948/5366/1017/files/20260115_0053_Image_Generation_remix_01kezestsbfs4avdn5p9enm423.png?v=1774642016",
     type: "grid"
